1bf3ede1025bb72321e911c7ece464ed597e3597
python <387675739@qq.com>
2015-11-09 14:03:43 +0800

    update code for sum of m~n

--- For.py
+++ For.py
@@ -1,9 +1,52 @@
-import math
+#!/usr/bin/python
 
-s=0
-m=0
-for i in range(0,100):
-	s=s+1
-	m=m+s
-print(m)
+import math
+import sys
+
+sum=0
+Result=0
+parameters=len(sys.argv)
+
+if(parameters<2):
+	print("Please input no less than 2 paraResulteters!")
+	exit()
+
+n_char = sys.argv[1]
+if(n_char.isdigit() == False):
+		print("Please input an integer!")
+		exit()
+		
+if(parameters==2):
+	for i in range(0,int(sys.argv[1])):
+		sum=sum+1
+		Result=Result+sum
+	print(Result)
+	
+if(parameters==3):
+	if(int(sys.argv[1])<int(sys.argv[2])):
+		for i in range(int(sys.argv[1]), int(sys.argv[2])+1):
+			Result=Result+i
+		print(Result)
+	else:
+		for i in range(int(sys.argv[2]), int(sys.argv[1])+1):
+			Result=Result+i
+		print(Result)
+
+if(parameters>3):
+	print("Please input no Resultore than 3 paraResulteters!")
+	exit()
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
 
