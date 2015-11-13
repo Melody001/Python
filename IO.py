90e95e90c483541e76366b88a74833be8b53551e
python <387675739@qq.com>
2015-11-13 17:55:26 +0800

    update code

--- IO.py
+++ IO.py
@@ -2,8 +2,15 @@
 import sys
 
 try:
-    f = open(str(sys.argv[1]), 'r')
-    print(str(f.read()))
+	f = open(str(sys.argv[1]), 'r')
+	while 1:
+		line=f.readline()
+		if line:
+			print (line)
+		if not line:
+			break
+		pass
+    #print(str(f.read()))
 finally:
     if f:
         f.close()
